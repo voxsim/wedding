cfcee75244eae47d8c1faf8a5441a3c84354f623
Simon Vocella <voxsim@gmail.com>
2022-02-20 19:20:45 +0100

    chore: remove blog and music-box

--- site/js/script.js
+++ site/js/script.js
@@ -582,20 +582,6 @@
 
 
     /*------------------------------------------
-        = TOGGLE MUSUC BIX
-    -------------------------------------------*/
-    if($(".music-box").length) {
-        var musicBtn = $(".music-box-toggle-btn"),
-            musicBox = $(".music-holder");
-
-        musicBtn.on("click", function() {
-            musicBox.toggleClass("toggle-music-box");
-            return false;
-        })
-    }
-
-
-    /*------------------------------------------
         = BACK TO TOP
     -------------------------------------------*/
     if($(".back-to-top-btn").length) {
